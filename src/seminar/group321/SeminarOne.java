9a5e38fa4a33b5d6ba55ecdaa512bc7bfc31ddf2
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2024-10-03 12:54:17 +0300

    Implementat procesare argumenete linie comanda

--- src/seminar/group321/SeminarOne.java
+++ src/seminar/group321/SeminarOne.java
@@ -1,7 +1,11 @@
 package seminar.group321;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
-Aceasta e cerinta pentru Seminar 1
+      Aceasta e cerinta pentru Seminar 1
+        
         1. Afisati minimul din lista de numere.
         2. Eliminati numerele prime din lista.
         3. Cautare numar dat de la tastatura
@@ -9,6 +13,37 @@ Aceasta e cerinta pentru Seminar 1
         4. Iesire
  */
 public class SeminarOne {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //Afisare: System.out.println("Ceva");
+        List<Integer> listaNumere;
+
+        if (args.length > 0) {
+            listaNumere = procesareListaArgumente(args);
+            System.out.println(listaNumere.toString());
+        }
+        else{
+            listaNumere = citesteTastatura();
+        }
+
+
+    }
+
+    private static List<Integer> citesteTastatura() {
+
+    }
+
+    private static List<Integer> procesareListaArgumente(String[] args) {
+        List<Integer> listaNumere = new ArrayList<>();
+        for (String arg : args) {
+            System.out.println("Un argument este: " + arg);
+            try {
+                int ai = Integer.parseInt(arg);
+                System.out.println(ai);
+                listaNumere.add(ai);
+            } catch (NumberFormatException ex) {
+                System.out.println("Argumentul " + arg + " nu a putut fi transformat in int.");
+            }
+        }
+        return listaNumere;
     }
 }
