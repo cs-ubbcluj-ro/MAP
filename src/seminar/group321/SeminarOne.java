9a5c7000766fb2738ab98e6b3b8ee4e4c9ae8f1e
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2024-10-03 12:57:34 +0300

    Completat cerinta Seminar 1

--- src/seminar/group321/SeminarOne.java
+++ src/seminar/group321/SeminarOne.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-      Aceasta e cerinta pentru Seminar 1
-        
+      Se lucreaza cu o lista de numere. Aceste numere se pot citi in doua moduri:
+         - din linia de comanda
+         - daca nu exista argumente in linia de comanda, de la tastatura
+      In orice mod ar fi citita lista, este necesara gestionarea cazului in care
+      se citeste si altceva decat un numar.
+
+      Scrieti o aplicatie cu un meniu care are are urmatoarele functionalitati:
         1. Afisati minimul din lista de numere.
         2. Eliminati numerele prime din lista.
         3. Cautare numar dat de la tastatura
