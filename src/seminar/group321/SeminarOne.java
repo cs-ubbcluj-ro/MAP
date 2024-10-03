c3c6e1ae58c7c8db827f7bb5ddc87f892eb42339
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2024-10-03 12:21:11 +0300

    Adaugat cerinta Seminar 1

--- src/seminar/group321/SeminarOne.java
+++ src/seminar/group321/SeminarOne.java
@@ -1,4 +1,14 @@
 package seminar.group321;
 
+/*
+Aceasta e cerinta pentru Seminar 1
+        1. Afisati minimul din lista de numere.
+        2. Eliminati numerele prime din lista.
+        3. Cautare numar dat de la tastatura
+        3. Gasiti cea mai lunga secventa de numere impare din lista.
+        4. Iesire
+ */
 public class SeminarOne {
+    public static void main(String[] args){
+    }
 }
