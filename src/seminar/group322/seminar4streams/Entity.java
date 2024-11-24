82c3f2d8dd8aaf393b4820c6b607d04b4a82367f
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2024-11-24 12:51:19 +0200

    Adaugat stream() examples

---
+++ src/seminar/group322/seminar4streams/Entity.java
@@ -0,0 +1,16 @@
+package seminar.group322.seminar4streams;
+
+
+import java.io.Serializable;
+
+//nu putem crea obiecte de tip Entity, clasa e abstracta
+public abstract class Entity implements Serializable {
+    protected int id;
+    public Entity(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
